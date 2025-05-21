9af464edbfce79d881fc607c1a04f307fd8dda29
Ragulstar100 <ragulstar100@gmail.com>
2025-05-21 14:10:56 +0530

    aas3 complete

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [a, setA] = useState([]); 
+
+  useEffect(() => {
+    fetch("words.json")
+      .then(res => res.json())
+      .then(data => setA(data)) 
+      .catch(err => console.log(err));
+  }, []);
+
+  let str="I am ragul"
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     {
+        str.split(" ").map((word, index) => {
+          return (
+            <div key={index}>
+              {word}={a.find((item) => item.name === word)?.partsOfSpeech ||"others"}
+            </div>
+          )
+        })
+      
+     }
+          
+    </>
   );
 }
 
 export default App;
+
+
---
+++ sample/src/test.js
@@ -0,0 +1,8 @@
+ export default function h(){
+    return (
+        <div>
+        <h1>Hello</h1>
+        <p>This is your first React component</p>
+        </div>
+    );
+}
